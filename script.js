e3420872679baf9cb2e74a63d60f8099880c8b8c
Shivanirathor <Shivanirathor034@gmail.com>
2024-04-02 17:30:45 +0530

    portfolio

--- script.js
+++ script.js
@@ -32,20 +32,20 @@ window.onscroll = () => {
     navbar.classList.remove('active');
 };
 
-ScrollReveal({
-    //reset:true,
-    distance:'80px',
-    duration:2000,
-    delay:200
-});
+// ScrollReveal({
+//     //reset:true,
+//     distance:'80px',
+//     duration:2000,
+//     delay:200
+// });
 
-ScrollReveal().reveal('.home-content,.heading',{origin:'top'});
-ScrollReveal().reveal('.home-img,.services-container,.portfolio-box,.contact form',{origin:'bottom'});
-ScrollReveal().reveal('.home-content h1,.about-img',{origin:'left'});
-ScrollReveal().reveal('.home-content p,.about-content',{origin:'right'});
+// ScrollReveal().reveal('.home-content,.heading',{origin:'top'});
+// ScrollReveal().reveal('.home-img,.services-container,.portfolio-box,.contact form',{origin:'bottom'});
+// ScrollReveal().reveal('.home-content h1,.about-img',{origin:'left'});
+// ScrollReveal().reveal('.home-content p,.about-content',{origin:'right'});
 
 const typed = new Typed('.multiple-text',{
-    strings:['CSE Student at NMIT ','Frontend developer'],
+    strings:['Web Developer ','Frontend Developer'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
